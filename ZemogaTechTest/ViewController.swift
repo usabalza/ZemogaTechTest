1d9dd46919e01f7e359f753fc8d284b1ef3df92d
Uziel Sabalza <usabalza@shokworks.io>
2022-06-30 09:34:36 -0400

    Initial Commit

---
+++ ZemogaTechTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ZemogaTechTest
+//
+//  Created by macbook  on 30/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
